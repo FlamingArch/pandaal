72912dae17fa82fbae5941cab50002abb918629f
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-09-03 02:13:07 +0530

    Added Missing Home Component

---
+++ pages/_home.js
@@ -0,0 +1,8 @@
+const PageHome = () => (
+  <>
+    <h1 className={styles.title}>
+      Welcome to <a href="https://pandaal.in">pandaal</a>
+    </h1>
+    <p className={styles.description}>Abhi Ban Rha Hai, Sabar Kro</p>
+  </>
+);

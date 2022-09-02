7d9deb85f8c0822d7b90aebb158efd1a0ab86283
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-09-03 02:11:00 +0530

    Added TailwindCSS

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import AppHead from "../components/head";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>pandaal: Event Ecosystem</title>
-        <meta
-          name="description"
-          content="Have the power to explore and be part of amazing events happening in your city."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://pandaal.in">pandaal</a>
-        </h1>
-
-        <p className={styles.description}>Abhi Ban Rha Hai, Sabar Kro</p>
-      </main>
-    </div>
+    <>
+      <AppHead />
+    </>
   );
 }

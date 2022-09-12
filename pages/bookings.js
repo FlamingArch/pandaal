77d61a9b8ec189171affaf1a217ec06b37e12604
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-09-12 07:57:37 +0530

    App Download Prompts

--- pages/bookings.js
+++ pages/bookings.js
@@ -1,7 +1,11 @@
 import { List } from "../components";
 
 const PageBookings = () => {
-  return <List.View>Bookings</List.View>;
+  return (
+    <List.View>
+      <List.Heading>Bookings</List.Heading>
+    </List.View>
+  );
 };
 
 export default PageBookings;

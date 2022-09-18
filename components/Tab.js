0a1f0605689ed3752d1b7954e2b20a89039a68d7
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-09-18 12:58:31 +0530

    Added Tab Components

---
+++ components/Tab.js
@@ -0,0 +1,48 @@
+import { createContext, useState, useContext } from "react";
+import styles from "../styles/Tab.module.scss";
+
+const Context = createContext();
+
+const Provider = ({ children }) => {
+  const [index, setIndex] = useState(0);
+
+  return (
+    <Context.Provider value={{ index: index, setIndex: setIndex }}>
+      {children}
+    </Context.Provider>
+  );
+};
+
+const Bar = ({ items }) => {
+  const { index, setIndex } = useContext(Context);
+
+  return (
+    <div className={styles.tabbar}>
+      {items.map((e, i) => (
+        <div
+          key={i}
+          className={
+            index == i ? `${styles.tab} ${styles.selected}` : styles.tab
+          }
+          onClick={() => setIndex(i)}
+        >
+          {e}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const View = ({ children }) => {
+  const { index } = useContext(Context);
+  return children[index];
+};
+
+const Tab = {
+  Context: Context,
+  Provider: Provider,
+  Bar: Bar,
+  View: View,
+};
+
+export default Tab;

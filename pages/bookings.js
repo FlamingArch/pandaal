6f061a736c61ed4e0e1a02d230e289ac6555b955
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-09-18 12:58:41 +0530

    Added Bookings Page

--- pages/bookings.js
+++ pages/bookings.js
@@ -1,10 +1,40 @@
-import { List } from "../components";
+import Image from "next/image";
+import { List, Navigation } from "../components";
+import Tab from "../components/Tab";
+import illustration from "../res/events.svg";
 
 const PageBookings = () => {
   return (
-    <List.View>
-      <List.Heading>Bookings</List.Heading>
-    </List.View>
+    <Tab.Provider>
+      <List.View>
+        <List.Heading>Booking History</List.Heading>
+        <Tab.Bar items={["Upcoming", "Older"]} />
+        <Tab.View>
+          <section className="flex flex-col p-6 pt-[20vh] text-center">
+            <Image
+              src={illustration}
+              alt=""
+              className="h-[50vh] scale-75 aspect-square"
+            />
+            <p className="text-xl font-bold center">No upcoming bookings.</p>
+            <p className="text-xl center">
+              You {"don't"} have any upcoming bookings.
+            </p>
+          </section>
+          <section className="flex flex-col p-6 pt-[20vh] text-center">
+            <Image
+              src={illustration}
+              alt=""
+              className="h-[50vh] scale-75 aspect-square"
+            />
+            <p className="text-xl font-bold center">No older bookings.</p>
+            <p className="text-xl center">
+              You {"don't"} have any older bookings.
+            </p>
+          </section>
+        </Tab.View>
+      </List.View>
+    </Tab.Provider>
   );
 };
 

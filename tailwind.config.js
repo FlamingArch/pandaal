76d3670c0b988ead83fb9b008661426380c3e81f
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-09-26 19:32:16 +0530

    Added Icons, Colors to tailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3F4882",
+        "primary-light": "#505BA5",
+        "primary-dark": "#2F3560",
+      },
+    },
   },
   plugins: [],
 };

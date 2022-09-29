882ce2fa93143e6ffcbc59034ed7d1fc6318371b
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-09-29 19:42:28 +0530

    Fixed Visual Bugs

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { AppHead } from "../fragments";
 import PageBookings from "./bookings";
 import PageFavourites from "./favourites";
 import PageNotifications from "./notifications";
-import PageNewEvent from "./newevent";
+import PageHome from "./_home";
 import Logo from "../fragments/Logo";
 import { useRef, useState } from "react";
 
@@ -42,25 +42,27 @@ export default function Home() {
 
   return (
     <Navigation.Provider>
-      <AppHead />
-      <Scaffold
-        logo={<Logo.Text />}
-        middle={
-          <Navigation.Bar style="navigation">
-            <Navigation.Item Icon={IconHome} label="Home" />
-            <Navigation.Item Icon={IconFavorites} label="Favourites" />
-            <Navigation.Item Icon={IconBookings} label="Bookings" />
-            <Navigation.Item Icon={IconNotifications} label="Notifications" />
-          </Navigation.Bar>
-        }
-      >
-        <Navigation.View>
-          <PageNewEvent />
-          <PageFavourites />
-          <PageBookings />
-          <PageNotifications />
-        </Navigation.View>
-      </Scaffold>
+      <FirebaseIntegration.Provider>
+        <AppHead />
+        <Scaffold
+          logo={<Logo.Text className="hidden-mobile" />}
+          middle={
+            <Navigation.Bar style="navigation">
+              <Navigation.Item Icon={IconHome} label="Home" />
+              <Navigation.Item Icon={IconFavorites} label="Favourites" />
+              <Navigation.Item Icon={IconBookings} label="Bookings" />
+              <Navigation.Item Icon={IconNotifications} label="Notifications" />
+            </Navigation.Bar>
+          }
+        >
+          <Navigation.View>
+            <PageHome />
+            <PageFavourites />
+            <PageBookings />
+            <PageNotifications />
+          </Navigation.View>
+        </Scaffold>
+      </FirebaseIntegration.Provider>
     </Navigation.Provider>
   );
 }

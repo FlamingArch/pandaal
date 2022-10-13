a791a4b0108900ee910ffd8edbc89b2647dcb3a4
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-10-13 22:56:10 +0530

    Added Package react-router

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Firebase from "./contexts/Firebase";
+import { BrowserRouter } from "react-router-dom";
 import "./styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Firebase.Provider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Firebase.Provider>
   </React.StrictMode>
 );

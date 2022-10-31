7eaff049f4d2d45fe353f910f887e1d2f913e0f4
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-10-31 22:29:48 +0530

    Updated to use dynamic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import { Scaffold } from "./components";
 import PageEvent from "./pages/Event";
 
 export default function App() {
+  const id = useParams().id;
+  console.log("Fetching event", id);
   return (
     <Scaffold.View>
-      <PageEvent
-        match="PyuUvuj3qF1RKQxluQIW"
-      />
+      <PageEvent match={id} />
     </Scaffold.View>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Firebase from "./contexts/Firebase";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Firebase.Provider>
       <BrowserRouter>
-        <App />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/:id" element={<App />} />
+        </Routes>
       </BrowserRouter>
     </Firebase.Provider>
   </React.StrictMode>
 );
+
+function Home() {
+  return (
+    <div className="w-screen h-screen grid place-content-center text-center">
+      <p className="font-extrabold text-6xl text-primary-400">pandaal</p>
+      <p className="text-primary-400 text-xl">
+        An event ecosystem
+      </p>
+    </div>
+  );
+}

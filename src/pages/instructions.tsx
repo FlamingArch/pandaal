c9417747bef0ab56d38d94dd10f602c234e5b593
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-11-29 01:25:09 +0530

    Instructions Page

--- src/pages/instructions.tsx
+++ src/pages/instructions.tsx
@@ -1,18 +1,15 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { Link, Outlet } from "react-router-dom";
-import { IconBack } from "../components/icons";
+import { Link, Outlet, useParams } from "react-router-dom";
 
-let backButton = (
-  <Link
-    to={-1}
-    className="cursor-pointer transition p-3 w-fit flex gap-3 rounded-xl bg-primary-50 hover:bg-primary-100 fill-black hover:fill-primary-500 hover:text-primary-500"
-  >
-    <IconBack className="w-6 h-6" />
-  </Link>
-);
+import { useEvent } from "../hooks";
+import { parseHTML } from "../helpers";
+import { BackButton } from "../fragments";
 
 export default function PageRegister() {
+  const { eventId } = useParams();
+  const event = useEvent(eventId ?? "");
+
   return (
     <motion.div
       animate={{ y: [window.innerHeight, 0] }}
@@ -23,9 +20,21 @@ export default function PageRegister() {
       }}
       className="fixed top-0 left-0 w-screen h-screen bg-white z-50 p-8 gap-8 flex flex-col"
     >
-      {backButton}
-      Hello, World
-      <Outlet />
+      {<BackButton />}
+      <div className="w-full md:w-1/2 xl:w-1/3 mx-auto gap-8 flex flex-col h-full">
+        <div className="flex-grow-0 md:flex-grow transition-all" />
+        <div className="text-3xl font-bold">Instructions</div>
+        {parseHTML(event?.howToRegisterHtmlText)}
+        <div className="flex-grow md:flex-grow-0 transition-all" />
+        <Link
+          to={`/${eventId}/instructions`}
+          className="grid place-content-center px-8 py-3 bg-primary-500 text-white rounded-2xl shadow-xl transition-all shadow-primary-300 dark:shadow-primary-700 hover:shadow-primary-500 hover:shadow-2xl hover:scale-105 hover:bg-primary-600"
+        >
+          Register
+        </Link>
+        <div className="flex-grow-0 md:flex-grow transition-all" />
+        <Outlet />
+      </div>
     </motion.div>
   );
 }

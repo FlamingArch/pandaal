a676b32641da3f39f7103ca5140aa1e55da2cecb
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-11-29 01:42:07 +0530

    Registration Page

--- src/pages/instructions.tsx
+++ src/pages/instructions.tsx
@@ -18,22 +18,21 @@ export default function PageRegister() {
         type: "spring",
         duration: 0.3,
       }}
-      className="fixed top-0 left-0 w-screen h-screen bg-white z-50 p-8 gap-8 flex flex-col"
+      className="fixed top-0 left-0 w-screen h-screen bg-white z-50 p-8 pb-20 flex flex-col"
     >
-      {<BackButton />}
+      <BackButton />
       <div className="w-full md:w-1/2 xl:w-1/3 mx-auto gap-8 flex flex-col h-full">
         <div className="flex-grow-0 md:flex-grow transition-all" />
         <div className="text-3xl font-bold">Instructions</div>
         {parseHTML(event?.howToRegisterHtmlText)}
         <div className="flex-grow md:flex-grow-0 transition-all" />
         <Link
-          to={`/${eventId}/instructions`}
+          to={`/${eventId}/register`}
           className="grid place-content-center px-8 py-3 bg-primary-500 text-white rounded-2xl shadow-xl transition-all shadow-primary-300 dark:shadow-primary-700 hover:shadow-primary-500 hover:shadow-2xl hover:scale-105 hover:bg-primary-600"
         >
           Register
         </Link>
         <div className="flex-grow-0 md:flex-grow transition-all" />
-        <Outlet />
       </div>
     </motion.div>
   );
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,30 +1,51 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { Link, Outlet } from "react-router-dom";
-import { IconBack } from "../components/icons";
-
-let backButton = (
-  <Link
-    to={-1}
-    className="cursor-pointer transition p-3 w-fit flex gap-3 rounded-xl bg-primary-50 hover:bg-primary-100 fill-black hover:fill-primary-500 hover:text-primary-500"
-  >
-    <IconBack className="w-6 h-6" />
-  </Link>
-);
+import { Link, Outlet, useParams } from "react-router-dom";
+import { BackButton } from "../fragments";
+import { generateForm } from "../helpers";
+import { useEvent } from "../hooks";
 
 export default function PageRegister() {
+  const { eventId } = useParams();
+  const event = useEvent(eventId ?? "");
+
+  const [response, setResponse] = React.useState([]);
+  const [enableButton, setEnableButton] = React.useState(false);
+
   return (
     <motion.div
-      animate={{ y: [window.innerHeight, 0] }}
-      exit={{ y: [0, window.innerHeight] }}
+      animate={{ opacity: [0, 1] }}
+      exit={{ opacity: [1, 0] }}
       transition={{
         type: "spring",
         duration: 0.3,
       }}
       className="fixed top-0 left-0 w-screen h-screen bg-white z-50 p-8 gap-8 flex flex-col"
     >
-      {backButton}
-      Hello, World
+      <BackButton />
+
+      <div className="w-full md:w-1/2 xl:w-1/3 mx-auto gap-8 flex flex-col h-full">
+        <div className="flex-grow-0 md:flex-grow transition-all" />
+        <div className="text-3xl font-bold">Fill Out This Form</div>
+        {event?.questions &&
+          generateForm(
+            event?.questions,
+            response,
+            setResponse,
+            setEnableButton
+          )}
+        <div className="flex-grow md:flex-grow-0 transition-all" />
+        {enableButton && (
+          <Link
+            to={`/${eventId}/register`}
+            className="grid place-content-center px-8 py-3 bg-primary-500 text-white rounded-2xl shadow-xl transition-all shadow-primary-300 dark:shadow-primary-700 hover:shadow-primary-500 hover:shadow-2xl hover:scale-105 hover:bg-primary-600"
+          >
+            Register
+          </Link>
+        )}
+        <div className="flex-grow-0 md:flex-grow transition-all" />
+      </div>
+
       <Outlet />
     </motion.div>
   );

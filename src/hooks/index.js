d2124f63425d850cd01b5e5edbb7776a86ebd875
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-12-19 15:56:52 +0530

    Display Events grouped by Category

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,3 @@
 export { default as useEvent } from "./useEvent";
 export { default as useEvents } from "./useEvents";
+export { default as useEventsCategorised } from "./useEventsCategorised";
--- src/pages/app/home.tsx
+++ src/pages/app/home.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { useEvents } from "../../hooks";
+import { useEvents, useEventsCategorised } from "../../hooks";
 import { AppBar, LoadingList, Scaffold, List, Button } from "../../components";
 import { AccountButton, EventCard } from "../../fragments";
 import { IconEdit, IconFeedback } from "../../components/icons";
 import { Outlet } from "react-router-dom";
 
 export default function PageHome() {
-  const events = useEvents();
+  const events = useEventsCategorised();
 
   let branding = (
     <div className="text-2xl font-bold text-primary-500">pandaal</div>
@@ -34,15 +34,19 @@ export default function PageHome() {
             </Button>
           }
         />
-        <List.Section heading="Events">
-          {events.length == 0 ? (
-            <LoadingList length={4} />
-          ) : (
-            events.map((e) => (
-              <EventCard event={e} key={e.id} hover details navigateOnClick />
-            ))
-          )}
-        </List.Section>
+        {Object.keys(events).map((category, index) => (
+          <List.Section heading={category} key={index}>
+            {events[category].map((event, index) => (
+              <EventCard
+                event={event}
+                key={event.id}
+                hover
+                details
+                navigateOnClick
+              />
+            ))}
+          </List.Section>
+        ))}
         <div className="h-56"></div>
       </List.View>
       <Outlet />

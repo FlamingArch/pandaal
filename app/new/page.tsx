6faf72412d7004b81337a82759254bce54bb7889
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2023-01-03 01:36:21 +0530

    Clean for Branch Replacement
    Cleaned Main Branch, Prep for Replacement from Recreate branch

--- app/new/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function () {
-  return <div>New Event Creation Form Page</div>;
-}
--- components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as LimitedParagraph } from "./limitedParagraph";
-export { default as Text } from "./text";
-export { default as Input } from "./input";

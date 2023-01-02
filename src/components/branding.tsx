9b50aac10eab98eb53aca185e53ab66f228ddc7b
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2023-01-03 01:49:50 +0530

    Revert "Recreate"

--- src/components/branding.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ViewBranding() {
-  return <div className="text-2xl font-bold text-primary-500">pandaal</div>;
-}
--- src/pages/test/components.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Testcomponents() {
-  return <div>Test Components Page</div>;
-}

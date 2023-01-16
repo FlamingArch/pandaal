08fedb179089dbb33e6d8477ef873ba4fdfb1327
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2023-01-17 03:05:14 +0530

    Fixed Blank Page when redirecting to Sign In

--- src/components/requireSignIn.jsx
+++ src/components/requireSignIn.jsx
@@ -1,24 +1,25 @@
 import React, { useContext } from "react";
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import { FirebaseContext } from "../contexts/firebase";
 import { IconPreloader } from "./icons";
 
 export default function RequireSignIn({ children }) {
   const { user, loggingIn } = useContext(FirebaseContext);
   const location = useLocation();
+  const navigate = useNavigate();
 
   console.log(loggingIn);
 
   if (loggingIn) {
-    <div className="w-screen h-screen bg-gray-50 flex flex-col items-center justify-center">
-      <IconPreloader className="w-8 h-8 stroke-gray-500" />
-    </div>;
+    return (
+      <div className="w-screen h-screen bg-gray-50 flex flex-col items-center justify-center">
+        <IconPreloader className="w-8 h-8 stroke-gray-500" />
+      </div>
+    );
   }
 
   if (!user) {
-    setTimeout(() => {
-      return <Navigate to="/signin" state={{ path: location.pathname }} />;
-    }, 1000);
+    return <Navigate to="/signin" state={{ path: location.pathname }} />;
   } else {
     return children;
   }

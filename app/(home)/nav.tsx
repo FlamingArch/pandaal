1bfebc1a5c9a1ab2f64d6027598d5d27e318a79d
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2023-02-18 10:55:36 +0530

    Added Slick Effects to Navigation

--- app/(home)/nav.tsx
+++ app/(home)/nav.tsx
@@ -12,13 +12,13 @@ import { usePathname } from "next/navigation";
 export default function Navigation() {
   const pathname = usePathname();
   return (
-    <nav className="m-6 mx-12 md:ml-6 md:m-0 p-4 flex md:flex-col rounded-full md:rounded-3xl bg-primary-50 fixed md:sticky md:top-[6rem] bottom-0 left-0 right-0 gap-4 justify-around md:justify-start h-fit md:gap-0 md:w-[192px]">
+    <nav className="m-6 backdrop-blur-xl backdrop-saturate-200 bg-opacity-80 mx-12 md:ml-6 md:m-0 p-4 flex md:flex-col rounded-full md:rounded-3xl bg-primary-50 fixed md:sticky md:top-[6rem] bottom-0 left-0 right-0 gap-4 justify-around md:justify-start h-fit md:gap-0 md:w-[192px]">
       <Link
         href={"/home"}
         className={
           "flex gap-3 p-2 items-center " +
           (pathname == "/home"
-            ? "text-primary-500 fill-primary-500"
+            ? "text-primary-500 fill-primary-500 animate-jump"
             : "text-gray-700 fill-gray-700")
         }
       >
@@ -30,7 +30,7 @@ export default function Navigation() {
         className={
           "flex gap-3 p-2 items-center " +
           (pathname == "/bookings"
-            ? "text-primary-500 fill-primary-500"
+            ? "text-primary-500 fill-primary-500 animate-jump"
             : "text-gray-700 fill-gray-700")
         }
       >
@@ -42,7 +42,7 @@ export default function Navigation() {
         className={
           "flex gap-3 p-2 items-center " +
           (pathname == "/favourites"
-            ? "text-primary-500 fill-primary-500"
+            ? "text-primary-500 fill-primary-500 animate-jump"
             : "text-gray-700 fill-gray-700")
         }
       >
@@ -54,7 +54,7 @@ export default function Navigation() {
         className={
           "flex gap-3 p-2 items-center " +
           (pathname == "/notifications"
-            ? "text-primary-500 fill-primary-500"
+            ? "text-primary-500 fill-primary-500 animate-jump"
             : "text-gray-700 fill-gray-700")
         }
       >

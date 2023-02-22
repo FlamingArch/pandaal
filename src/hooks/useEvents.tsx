382f2d13011101b81a93670565cf413e0df0cf4b
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2023-02-22 16:41:44 +0530

    Display Events by City

--- src/hooks/useEvents.tsx
+++ src/hooks/useEvents.tsx
@@ -1,9 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { collection, getFirestore, query } from "firebase/firestore";
+import {
+  collection,
+  getFirestore,
+  query,
+  QueryConstraint,
+} from "firebase/firestore";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import constants from "../constants";
 
-export default function useEvents(queries = []) {
+export default function useEvents(queries: QueryConstraint[] = []) {
   const firestore = getFirestore(initializeApp(constants.firebaseConfig));
   return useCollectionData(query(collection(firestore, "Events"), ...queries));
 }
--- src/pages/app/home.tsx
+++ src/pages/app/home.tsx
@@ -1,23 +1,31 @@
 import React from "react";
-import { useEvents, useUserDoc } from "../../hooks";
+import { useEvents } from "../../hooks";
 import { AppBar, Scaffold, Page } from "../../components";
 import { AccountButton, Branding, EventCard } from "../../fragments";
 import { IconLocationEdit, IconPreloader } from "../../components/icons";
 import { useOutlet } from "react-router-dom";
 import { FirebaseContext } from "../../contexts/firebase";
 import { groupData } from "../../helpers";
+import { where } from "firebase/firestore";
 
 export default function PageHome() {
+  const today = new Date().toISOString().slice(0, 10).replace(/-/g, ""); // Today date in YYYYMMDD string
   const { user } = React.useContext<any>(FirebaseContext);
-  const [events, loading, error] = useEvents();
   const [groupedEvents, setGroupedEvents] = React.useState({});
+  const [city, setCity] = React.useState("Greater Noida");
+
+  const queries = [
+    where("active", "==", true),
+    where("acceptingRegistrations", "==", true),
+    where("startDate", ">=", today),
+  ];
+  const [events, loading, error] = useEvents(queries);
 
   React.useEffect(() => {
     if (events) {
       setGroupedEvents(groupData(events, "Category"));
     }
   }, [events]);
-
   const outlet = useOutlet();
   window.document.title = "Pandaal: An Event Ecosystem";
 
@@ -35,13 +43,22 @@ export default function PageHome() {
       overlay={outlet}
       leading={
         <div className="flex flex-col p-6 gap-2">
-          <p>Hey {`${user?.displayName.split(" ")[0] ?? "Wandrer"}`}</p>
+          <p>Hey {`${user?.displayName.split(" ")[0] ?? "Wanderer"}`}</p>
           <p className="text-2xl font-bold">
             Showing all the getaway spots near
           </p>
-          <p className="flex text-2xl font-bold p-4 rounded-2xl transition-colors hover:bg-secondary-50 text-secondary-500 fill-secondary-500">
-            Greater Noida <IconLocationEdit className="w-8 h-8" />
-          </p>
+          <div className="flex text-2xl font-bold p-4 rounded-2xl transition-colors hover:bg-secondary-50 text-secondary-500 fill-secondary-500">
+            <select
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              className="bg-transparent select-hide-arrow"
+            >
+              <option value="Greater Noida">Greater Noida</option>
+              <option value="Delhi">Delhi</option>
+              <option value="Lucknow">Lucknow</option>
+            </select>
+            <IconLocationEdit className="w-8 h-8" />
+          </div>
         </div>
       }
     >
@@ -52,25 +69,32 @@ export default function PageHome() {
           </div>
         ) : (
           Object.keys(groupedEvents).map((category, index) => {
-            return (
-              <div
-                key={index}
-                className="flex flex-col w-screen overflow-hidden items-stretch"
-              >
-                <p className="p-6 pb-0">{category}</p>
-                <div className="flex w-screen p-6 gap-6 overflow-hidden hover:overflow-scroll">
-                  {groupedEvents[category].map((event, index) => (
-                    <EventCard
-                      event={event}
-                      key={event.id}
-                      hover
-                      details
-                      navigateOnClick
-                    />
-                  ))}
+            if (
+              groupedEvents[category].filter(
+                (e) => e.City == city || e.City == ""
+              ).length > 0
+            )
+              return (
+                <div
+                  key={index}
+                  className="flex flex-col w-screen overflow-hidden items-stretch"
+                >
+                  <p className="p-6 pb-0">{category}</p>
+                  <div className="flex w-screen p-6 gap-6 overflow-hidden hover:overflow-scroll">
+                    {groupedEvents[category].map((event, index) => {
+                      return (
+                        <EventCard
+                          event={event}
+                          key={event.id}
+                          hover
+                          details
+                          navigateOnClick
+                        />
+                      );
+                    })}
+                  </div>
                 </div>
-              </div>
-            );
+              );
           })
         )}
       </Page>

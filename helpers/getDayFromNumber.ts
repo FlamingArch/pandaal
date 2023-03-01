2836e18a8201c87df72cc06b0a39c5c0b3618dec
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2023-03-02 00:52:14 +0530

    Date Helpers

---
+++ helpers/getDayFromNumber.ts
@@ -0,0 +1,11 @@
+export default function (n: number) {
+  return [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ][n];
+}
--- helpers/index.ts
+++ helpers/index.ts
@@ -1,1 +1,6 @@
 export { default as groupData } from "./groupData";
+export { default as getDateString } from "./getDateString";
+export { default as getDayFromNumber } from "./getDayFromNumber";
+export { default as getFullDateRange } from "./getFullDateRange";
+export { default as getLongDate } from "./getLongDate";
+export { default as getMonthFromNumber } from "./getMonthFromNumber";

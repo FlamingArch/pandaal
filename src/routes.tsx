033a3619d1b2e7c04ddb93daf34d6d89724e705c
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2023-03-23 17:20:12 +0530

    Regenerate Project w/ React TS + SWC

---
+++ src/routes.tsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import CityPage from "./app/city/page";
+import { Input } from "./components";
+import { getCityStorage, setCityStorage } from "./functions";
+
+export default function AppRoutes() {
+  return <></>;
+}

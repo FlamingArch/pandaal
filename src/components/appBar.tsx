797f096a822ac686a920cde80e9d785bf906f294
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2023-06-12 00:44:58 +0530

    New AppBar

--- src/components/appBar.tsx
+++ src/components/appBar.tsx
@@ -1,155 +1,175 @@
+import React from "react";
+import { AppBarProps } from "../types/appBar";
+
+const appBarStylesClasses = {
+  root: "",
+  container: "flex flex-col",
+  toolbar: "flex flex-row items-center",
+  leading: "",
+  title: "font-medium",
+  center: "flex-grow flex items-center justify-center",
+  actions: "flex",
+  primaryBar: "flex items-center",
+  heading: "flex text-xl flex-grow font-medium",
+  primary: "p-3",
+  backdrop: {
+    clear: "",
+    gradient: "bg-gradient-to-b from-page dark:from-page-dark to-transparent",
+    gradientReverse:
+      "bg-gradient-to-b from-page-dark dark:from-page to-transparent",
+    gradientBlack: "bg-gradient-to-b from-page-dark to-transparent",
+    material: "backdrop-blur-xl backdrop-saturate-150",
+    materialShadow: "backdrop-blur-xl backdrop-saturate-150 shadow-xl",
+    solid: "bg-page dark:bg-page-dark",
+    shadow: "bg-page dark:bg-page-dark shadow-xl",
+  },
+};
+
 export default function AppBar({
   leading,
   title,
-  actions,
   center,
+  actions,
+  heading,
+  primary,
   children,
-  backdrop,
-  background,
   responsive,
   sticky,
+  backdrop,
+  background,
   className,
+  cornerRadius,
   padding,
   margin,
-  cornerRadius,
   gap,
-}: {
-  leading?: React.ReactNode;
-  title?: React.ReactNode;
-  actions?: React.ReactNode | React.ReactNode[];
-  center?: React.ReactNode | React.ReactNode;
-  children?: React.ReactNode;
-  responsive?: boolean;
-  sticky?: boolean | "bottom";
-  backdrop?:
-    | "clear"
-    | "gradient"
-    | "gradientReverse"
-    | "gradientBlack"
-    | "material"
-    | "materialShadow"
-    | "solid"
-    | "shadow";
-  background?:
-    | "clear"
-    | "gradient"
-    | "gradientReverse"
-    | "gradientBlack"
-    | "material"
-    | "materialShadow"
-    | "solid"
-    | "shadow";
-  className?: string;
-  cornerRadius?:
-    | number
-    | {
-        topLeft?: number;
-        topRight?: number;
-        bottomLeft?: number;
-        bottomRight?: number;
-      };
-  padding?:
-    | number
-    | { top?: number; right?: number; bottom?: number; left?: number };
-  margin?:
-    | number
-    | { top?: number; right?: number; bottom?: number; left?: number };
-  gap?: number;
-}) {
-  const getBackdropStyles = (backdrop?: string) => {
-    switch (backdrop) {
-      case "clear":
-        return "bg-transparent";
-      case "gradient":
-        return (
-          "from-[#FFFFFFCC] to-transparent " +
-          (sticky === "bottom" ? "bg-gradient-to-t" : "bg-gradient-to-b")
-        );
-      case "gradientReverse":
-        return (
-          "from-[#000000CC] to-transparent " +
-          (sticky === "bottom" ? "bg-gradient-to-t" : "bg-gradient-to-b")
-        );
-      case "gradientBlack":
-        return (
-          "from-[#000000CC] to-transparent " +
-          (sticky === "bottom" ? "bg-gradient-to-t" : "bg-gradient-to-b")
-        );
-      case "material":
-      default:
-        return "bg-white bg-opacity-80 backdrop-filter backdrop-saturate-150 backdrop-blur-lg";
-      case "materialShadow":
-        return "bg-white bg-opacity-80 backdrop-filter backdrop-saturate-150 backdrop-blur-lg shadow-lg";
-      case "solid":
-        return "bg-white";
-      case "shadow":
-        return "bg-white shadow-lg";
-        return "bg-white";
-    }
+  style,
+  classNames,
+  styles,
+}: AppBarProps) {
+  const containerStyles = {
+    borderRadius:
+      typeof cornerRadius == "number"
+        ? `${cornerRadius / 4}rem`
+        : `${(cornerRadius?.topLeft ?? 0) / 4}rem ${
+            (cornerRadius?.topRight ?? 0) / 4
+          }rem ${(cornerRadius?.bottomLeft ?? 0) / 4}rem ${
+            (cornerRadius?.bottomRight ?? 0) / 4
+          }rem`,
+    padding:
+      typeof padding == "number"
+        ? `${(padding ?? 4) / 4}rem`
+        : `${(padding?.top ?? 4) / 4}rem ${(padding?.right ?? 4) / 4}rem ${
+            (padding?.bottom ?? 4) / 4
+          }rem ${(padding?.left ?? 4) / 4}rem`,
+    margin:
+      typeof margin == "number"
+        ? `${(margin ?? 0) / 4}rem`
+        : `${(margin?.top ?? 0) / 4}rem ${(margin?.right ?? 0) / 4}rem ${
+            (margin?.bottom ?? 0) / 4
+          }rem ${(margin?.left ?? 0) / 4}rem`,
+    gap: gap ?? `1rem`,
+    ...styles?.container,
   };
 
-  const responsiveStyles = responsive ? " md:w-2/3 lg:w-1/2 mx-auto" : "";
+  const responsiveStylesContainer = responsive ? "md:w-2/3 lg:w-1/2" : "";
+  const responsiveStylesRoot = responsive ? "flex md:justify-center" : "";
 
-  return (
-    <div
-      className={`transition-all ${getBackdropStyles(backdrop ?? "clear")} ${
-        sticky === true
-          ? "sticky top-0"
-          : sticky === "bottom"
-          ? "sticky bottom-0"
-          : ""
+  const stickyStyles = sticky
+    ? `sticky ${sticky == "bottom" ? "bottom-0" : "top-0"}`
+    : "";
+
+  const toolbar = (
+    <section
+      style={{ gap: gap ?? `1rem` }}
+      className={`${`${appBarStylesClasses.toolbar} ${classNames?.toolbar}`} ${
+        classNames?.toolbar
       }`}
     >
       <div
-        style={{
-          borderRadius:
-            typeof cornerRadius == "number"
-              ? `${cornerRadius / 4}rem`
-              : `${(cornerRadius?.topLeft ?? 0) / 4}rem ${
-                  (cornerRadius?.topRight ?? 0) / 4
-                }rem ${(cornerRadius?.bottomLeft ?? 0) / 4}rem ${
-                  (cornerRadius?.bottomRight ?? 0) / 4
-                }rem`,
-          padding:
-            typeof padding == "number"
-              ? `${(padding ?? 6) / 4}rem`
-              : `${(padding?.top ?? 6) / 4}rem ${
-                  (padding?.right ?? 6) / 4
-                }rem ${(padding?.bottom ?? 6) / 4}rem ${
-                  (padding?.left ?? 6) / 4
-                }rem`,
-          margin:
-            typeof margin == "number"
-              ? `${(margin ?? 0) / 4}rem`
-              : `${(margin?.top ?? 0) / 4}rem ${(margin?.right ?? 0) / 4}rem ${
-                  (margin?.bottom ?? 0) / 4
-                }rem ${(margin?.left ?? 0) / 4}rem`,
-          gap: `${(gap ?? 0) / 4}rem`,
-        }}
-        className={`flex flex-col flex-grow ${getBackdropStyles(
-          background
-        )} ${className}`}
+        style={{ gap: gap, ...styles?.leading }}
+        className={`${appBarStylesClasses.leading} ${classNames?.leading}`}
       >
-        <div
-          style={{ gap: `${(gap ?? 0) / 4}rem` }}
-          className={"flex w-full " + responsiveStyles}
-        >
-          <div
-            style={{
-              gap: `${(gap ?? 0) / 4}rem`,
-            }}
-            className="flex items-center"
-          >
-            {leading}
-            <div className="font-semibold">{title}</div>
-          </div>
-          <div className="flex items-center flex-grow justify-center">
-            {center}
-          </div>
-          <div className="flex items-center gap-4 justify-end">{actions}</div>
-        </div>
-        {children}
+        {leading}
       </div>
-    </div>
+      <div
+        style={{ gap: gap, ...styles?.title }}
+        className={`${appBarStylesClasses.title} ${classNames?.title}`}
+      >
+        {title}
+      </div>
+      <div
+        style={{ gap: gap, ...styles?.center }}
+        className={`${appBarStylesClasses.center} ${classNames?.center}`}
+      >
+        {center}
+      </div>
+      <div
+        style={{ gap: gap ?? "1rem", ...styles?.actions }}
+        className={`${appBarStylesClasses.actions} ${classNames?.actions}`}
+      >
+        {actions}
+      </div>
+    </section>
+  );
+
+  const primaryBar = (
+    <section
+      style={{ gap: gap ?? `1rem`, ...styles?.primaryBar }}
+      className={`${appBarStylesClasses.primaryBar} ${classNames?.primaryBar}`}
+    >
+      <h3
+        style={{
+          gap: gap ?? `1rem`,
+          ...styles?.heading,
+          padding: `0.75rem`,
+        }}
+        className={`${appBarStylesClasses.heading} ${classNames?.heading}`}
+      >
+        {heading}
+      </h3>
+      <main
+        style={{ gap: gap ?? `1rem`, ...styles?.primary }}
+        className={`${appBarStylesClasses.primary} ${classNames?.primary}`}
+      >
+        {primary}
+      </main>
+    </section>
+  );
+
+  return (
+    <header
+      style={{
+        ...styles?.root,
+        ...style,
+      }}
+      className={`${appBarStylesClasses.root} ${
+        classNames?.root
+      } ${className} ${responsiveStylesRoot} ${stickyStyles} ${
+        sticky === "bottom"
+          ? appBarStylesClasses.backdrop[backdrop ?? "material"].replace(
+              "bg-gradient-to-b ",
+              "bg-gradient-to-t "
+            )
+          : appBarStylesClasses.backdrop[backdrop ?? "material"]
+      }`}
+    >
+      <div
+        style={containerStyles}
+        className={`${appBarStylesClasses.container} ${
+          classNames?.container
+        } ${responsiveStylesContainer} ${
+          sticky === "bottom"
+            ? appBarStylesClasses.backdrop[background ?? "material"].replace(
+                "bg-gradient-to-b ",
+                "bg-gradient-to-t "
+              )
+            : appBarStylesClasses.backdrop[background ?? "material"]
+        }}`}
+      >
+        {(leading || title || center || actions) && toolbar}
+        {(heading || primary) && primaryBar}
+        {children && children}
+      </div>
+    </header>
   );
 }
